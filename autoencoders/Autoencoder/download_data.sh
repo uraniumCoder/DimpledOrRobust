30617219ea2117edcd77ff8e9619c55a8e5026c8
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-11-30 19:44:29 -0500

    script to download data

---
+++ autoencoders/Autoencoder/download_data.sh
@@ -0,0 +1,5 @@
+mkdir data
+cd data
+wget http://ai.stanford.edu/~jkrause/car196/cars_train.tgz
+wget http://ai.stanford.edu/~jkrause/car196/cars_test.tgz
+wget http://ai.stanford.edu/~jkrause/cars/car_devkit.tgz
