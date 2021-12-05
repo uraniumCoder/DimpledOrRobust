5eddc7b5433947bf5c4d1ea9982bee6483fc6762
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-05 14:07:13 -0500

    local manifold approx

---
+++ local_manifold.py
@@ -0,0 +1,14 @@
+import torch
+
+def get_local_approximation(encoder, decoder, image):
+  """
+  Gets set of vectors spanning the tangent space at image of the image manifold
+  """
+  z = encoder(image[None, :, :, :])
+  z_flat = z.reshape((1, -1))
+  image_on_manifold = decoder(z)
+  eps = 0.01
+  z_perturbed_flat = torch.eye(z_flat.shape[1], device='cuda')*eps + z_flat
+  z_perturbed = z_perturbed_flat.reshape((-1, *z.shape[1:]))
+  image_perturbed = decoder(z_perturbed)
+  return image_perturbed - image_on_manifold
