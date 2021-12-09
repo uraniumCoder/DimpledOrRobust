19889256e4669a47fa7e363d2e5ed3b583c18ff9
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-09 00:15:48 -0500

    improve numerical stability

--- local_manifold.py
+++ local_manifold.py
@@ -12,7 +12,7 @@ def get_local_approximation(encoder, decoder, image):
     z_perturbed_flat = torch.eye(z_flat.shape[1], device='cuda')*eps + z_flat
     z_perturbed = z_perturbed_flat.reshape((-1, *z.shape[1:]))
     image_perturbed = decoder(z_perturbed)
-    return (image_perturbed - image_on_manifold).detach()
+    return (image_perturbed - image_on_manifold).detach() / eps
 
 def get_orthonormal_basis(tangent_vectors):
     """
