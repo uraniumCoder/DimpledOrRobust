dc31fae1a5f4bc190b375f6704de5498801d8e3c
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-09 16:59:40 -0500

    testing functions

---
+++ testing/reconstruction.py
@@ -0,0 +1,49 @@
+"""
+Calculates the average L2 distance between images and reconstructed images by an autoencoder.
+"""
+from DimpledOrRobust.data_preprocessing.cifar10 import DatasetMaker
+import torch
+from matplotlib import pyplot as plt
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from data_preprocessing.cifar10 import Cifar10DatasetFiltered
+denormalize = Cifar10DatasetFiltered.denormalize
+
+def show_reconstructed_images(model, dataloader, save_path):
+    """
+    Show 5 sample images and corresponding reconstructed images
+    """
+
+    fig = plt.figure(figsize=(25, 10))
+    ax_arr = fig.subplots(2, 5)
+
+    plt.axis('off')
+    for X, y in dataloader:
+        for i in range(5):
+            image_orig = torch.permute(denormalize(X.cuda()), (0,2,3,1))
+            image_regen = torch.permute(torch.clamp(denormalize(model(X.cuda())[0]), 0, 1), (0,2,3,1))
+            ax_arr[0][i].imshow(image_orig[i].cpu().numpy())
+            ax_arr[1][i].imshow(image_regen[i].cpu().detach().numpy())
+        break
+    ax_arr[0][2].set_title('Original CIFAR-10 Images')
+    ax_arr[1][2].set_title('Reconstructed Images')
+    fig.tight_layout()
+    plt.savefig(save_path)
+
+def average_l2_dist(model, dataloader):
+    total_l2 = 0
+    count = 0
+    model.eval()
+    for X, y in dataloader:
+        image_orig = denormalize(X.cuda())
+        image_regen = torch.clamp(denormalize(model(X.cuda())[0]), 0, 1)
+        err = image_regen - image_orig
+
+        l2_dist = torch.sqrt((err**2).sum(dim=1).sum(dim=1).sum(dim=1))
+        total_l2 += l2_dist.sum().item()
+        count += X.shape[0]
+        
+    print('Average L2 Distance: ', total_l2 / count)
+    return total_l2 / count
---
+++ testing/tsne.py
@@ -0,0 +1,48 @@
+"""
+Visualize the latent space of a VAE using TSNE.
+"""
+import matplotlib.pyplot as plt
+import torch
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+def draw_digits_latent_space(z_mean_test, test_label, ax):
+    scatter = ax.scatter(z_mean_test[:,0],z_mean_test[:,1],c=test_label)#,cmap=plt.cm.get_cmap('jet', 10))
+    plt.colorbar(scatter, ax=ax)
+
+PERPLEXITIES = [2, 5, 10, 50, 100, 500]
+
+def tsne_plots_model(vae_model, dataloader, save_path, perplexities=PERPLEXITIES):
+    """
+    TSNE plots for the VAE model.
+    This function projects onto 150 dimensions first using PCA, then uses TSNE to project onto 2 dimensions.
+    """
+    fig = plt.figure(figsize=(30, 15))
+    fig.tight_layout()
+    ax_arr = fig.subplots(2, 3)
+    axs = [x for y in ax_arr for x in y]
+
+    Z_all = []
+    labels = []
+    with torch.no_grad():
+        for X, y in dataloader:
+            Z, _ = vae_model.encode(X.cuda())
+            Z_all.append(Z.cpu())
+            labels.append(y)
+
+    Z_stacked = torch.cat(Z_all, dim=0)
+    labels_stack = torch.cat(labels, dim=0)
+
+
+    for i, purp in enumerate(perplexities):
+        pca = PCA(n_components=150)
+        tsne = TSNE(n_components=2, random_state=42, perplexity=purp)
+
+        z_pca = pca.fit_transform(Z_stacked[:])
+        z_tsne = tsne.fit_transform(z_pca)
+
+        draw_digits_latent_space(z_tsne, labels_stack[:], axs[i])
+        axs[i].set_title(f'T-SNE Perplexity {purp}')
+    plt.show()
+
+    plt.savefig(save_path)
