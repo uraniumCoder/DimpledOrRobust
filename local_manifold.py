d1a00458fd59c91371fda9df47fb9d6d2cc453a5
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-09 00:30:38 -0500

    stable projections

--- local_manifold.py
+++ local_manifold.py
@@ -1,6 +1,6 @@
 import torch
 
-def get_local_approximation(encoder, decoder, image):
+def get_local_approximation(encoder, decoder, image, eps=None):
     """
     Gets set of vectors spanning the tangent space at image of the image manifold
     image is a tensor of shape (3, H, W)
@@ -8,7 +8,8 @@ def get_local_approximation(encoder, decoder, image):
     z = encoder(image[None, :, :, :])
     z_flat = z.reshape((1, -1))
     image_on_manifold = decoder(z)
-    eps = 0.01
+    if eps is None:
+        eps = 0.01
     z_perturbed_flat = torch.eye(z_flat.shape[1], device='cuda')*eps + z_flat
     z_perturbed = z_perturbed_flat.reshape((-1, *z.shape[1:]))
     image_perturbed = decoder(z_perturbed)
--- projection.py
+++ projection.py
@@ -3,15 +3,20 @@ import torch
 def projection(b, spanning_vector_matrix):
     #spanning vector matrix is of shape p,m
     #b is of shape n,1. Here p<n We stack zeros under A so that it becomes a matrix of shape n,m.
+
     b = b.type(torch.cuda.FloatTensor)
     n = b.shape[0]
     (p,m)= spanning_vector_matrix.shape
     zero_matrix = torch.zeros(n-p,m, device=b.device)
     A = torch.cat((spanning_vector_matrix,zero_matrix),dim=0)
+
+    #A is of shape n,m
+    """
     A_T = torch.transpose(A,0,1)
     inv = torch.linalg.inv(torch.matmul(A_T,A))
     mat1 = torch.matmul(A,inv)
     mat2 = torch.matmul(mat1,A_T)
+    """
+    coeffs = torch.linalg.lstsq(A,b)[0]
 
-    return torch.matmul(mat2,b)
-
+    return torch.matmul(A,coeffs)
