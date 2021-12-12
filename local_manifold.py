102609f0fcd97b62a51d3f1e316c81db79fab350
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-12 00:19:02 -0500

    bugfix

--- local_manifold.py
+++ local_manifold.py
@@ -7,8 +7,12 @@ def get_local_approximation(encoder, decoder, image, eps=None, batch_size=None):
     image is a tensor of shape (3, H, W)
     """
     z = encoder(image[None, :, :, :])
+    if type(z) is tuple:
+        z = z[0]
     z_flat = z.reshape((1, -1))
     image_on_manifold = decoder(z)
+    if type(image_on_manifold) is tuple:
+        image_on_manifold = image_on_manifold[0]
     if eps is None:
         eps = 0.01
     z_perturbed_flat = torch.eye(z_flat.shape[1], device='cuda')*eps + z_flat
@@ -21,6 +25,8 @@ def get_local_approximation(encoder, decoder, image, eps=None, batch_size=None):
         end = min(i*batch_size+batch_size, z_perturbed_flat.shape[0])
         z_perturbed = z_perturbed_flat[i*batch_size:end, :].reshape((-1, *z.shape[1:]))
         image_on_manifold_perturbed = decoder(z_perturbed)
+        if type(image_on_manifold_perturbed) is tuple:
+            image_on_manifold_perturbed = image_on_manifold_perturbed[0]
         image_perturbed[i*batch_size:end, :, :, :] = image_on_manifold_perturbed.detach()
 
     return (image_perturbed - image_on_manifold).detach() / eps
