6a06c13912558397ab918fd7572286a5b972270c
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-12 00:10:29 -0500

    synthetic cifar optimization

--- data_preprocessing/synthetic_cifar10.py
+++ data_preprocessing/synthetic_cifar10.py
@@ -20,17 +20,18 @@ class SyntheticCifar10Dataset(Dataset):
     TRANSFORM_NO_AUG = transforms.Compose([NRM])
 
 
-    def __init__(self, root):
+    def __init__(self, root, n_images):
         super().__init__()
         self.root_pth = Path(root)
-        self.image_pths = list(self.root_pth.glob('image_*.npy'))
-        self.image_idxs = [int(pth.stem.split('_')[1]) for pth in self.image_pths]
-        
+        #self.image_pths = list(self.root_pth.glob('image_*.npy'))
+        #self.image_idxs = [int(pth.stem.split('_')[1]) for pth in self.image_pths]
+        self.image_idxs = list(range(n_images))
+
     def __len__(self):
         return len(self.image_idxs)
 
     def __getitem__(self, idx):
-        image = np.load(self.root_pth / self.image_pths[idx])
+        image = np.load(self.root_pth / f'image_{self.image_idxs[idx]}.npy')
         image = torch.from_numpy(image).float()
         image = self.TRANSFORM_NO_AUG(image)
 
